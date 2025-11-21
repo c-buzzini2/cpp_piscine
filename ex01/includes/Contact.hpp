9816c83a22519fbb90771611ff79312b54d6afd6
Claudia <cbuzzini@student.42lisboa.com>
2025-11-21 15:59:16 +0000

    ex00 and beginning of ex01

---
+++ ex01/includes/Contact.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbuzzini <cbuzzini@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/20 13:53:36 by cbuzzini          #+#    #+#             */
+/*   Updated: 2025/11/21 15:20:32 by cbuzzini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include "PhoneBook.hpp"
+#include <iostream>
+
+class Contact
+{
+	public:
+		//Contact(void);
+		//~Contact(void);
+		//functions to add and search contacts
+	
+	private:
+		std::string	_phone_nb;
+		std::string	_f_name;
+		std::string	_l_name;
+		std::string	_nick;
+		std::string	_secret;
+
+};
+
+#endif
